9fcd159d93b7ef23f3d95d68af997b1614d15dcf
Wesley Isaias <wesley_isaias@hotmail.com>
2023-01-21 18:45:44 -0300

    first commit

---
+++ 2-FullStack Raiz/Aula04.js
@@ -0,0 +1,18 @@
+const lista = ['arroz, feijão', 'salva',]
+const collection = [
+
+    {id: '123', name : 'Ayrton'},
+    {id: '223', name: 'José'}
+];
+
+const user = { name : 'Ayrton'};
+
+console.log(collection.find(u => u.id === '223'));
+
+const users = {
+    '123': {id : '123', name : 'Ayrton'},
+    '223': {id : '223', name : 'José'}
+};
+
+console.log ['123']
+console.log (collection.length)
---
+++ 2-FullStack Raiz/Aula05.js
@@ -0,0 +1,26 @@
+const mapa = new Map ([
+    [0, 'Progamador'],
+    [1, 'a'],
+    [2, 'Bordo']
+]);
+const obj = {};
+mapa.set('3', 'wesley isaias');
+mapa.set(true, 'React');
+mapa.set({}, 'vazio');
+mapa.set(obj, 'Objeto vazio')
+/*
+console.log(mapa.get(0));  // .get é o metedo utilizado para acessar os valores em new Map
+console.log(mapa.get(true));
+console.log(mapa.get({})); // undefined porque eles são alocados em locais diferentes na memória. São diferentes.
+console.log(mapa.get(obj)); // passagem por referencia para objetos ou funções
+console.log(mapa.size);  //size -> Vai mostrar quantos elementos possuem ( o tamanho)*/
+/*outros metodos
+has-delete-clear-forEach-keys-values-entries*/
+
+mapa.forEach(function(value,key){
+   // console.log(value);
+})
+
+for (let m of mapa){
+    console.log(m);
+}
